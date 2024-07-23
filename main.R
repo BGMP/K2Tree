4b466174f1f8f3f5f90d27313d5b2587eb47440b
BGMP <jose@bgm.cl>
2024-07-23 02:55:41 -0400

    r: add space plot

--- main.R
+++ main.R
@@ -1,15 +1,11 @@
-# Load necessary libraries
 library(ggplot2)
-library(readr)
+library(scales)
 
-# Read the CSV file
 data <- read_csv("data/time.csv")
 
-# Define breakpoints for n axis
 n_breakpoints <- c(4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096)
 
-# Create the plot
-ggplot(data) +
+time_plot <- ggplot(data) +
   geom_point(aes(x = n, y = bitmatrix, color = "BitMatrix"), size = 2) +
   geom_point(aes(x = n, y = k2tree, color = "K2Tree"), size = 2) +
   geom_smooth(aes(x = n, y = bitmatrix, color = "BitMatrix"), method = "loess") +
@@ -22,5 +18,33 @@ ggplot(data) +
        color = "Legend") +
   theme_minimal() +
   scale_color_manual(values = c("BitMatrix" = "green", "K2Tree" = "orange")) +
-  # Add the breakpoints to the x-axis
-  scale_x_log10(breaks = n_breakpoints) 
+  scale_x_log10(breaks = n_breakpoints)
+
+ggsave("img/TIME.png", plot = time_plot, width = 10, height = 6, dpi = 300)
+
+data <- read.csv("data/space.csv")
+
+data$bitmatrix <- as.numeric(gsub("a", "", as.character(data$bitmatrix)))
+data$k2tree <- as.numeric(gsub("a", "", as.character(data$k2tree)))
+data <- na.omit(data)
+
+space_plot <- ggplot(data) +
+  geom_line(aes(x = n, y = bitmatrix, color = "BitMatrix"), size = 1) +
+  geom_point(aes(x = n, y = bitmatrix, color = "BitMatrix"), size = 3) +
+  geom_line(aes(x = n, y = k2tree, color = "K2Tree"), size = 1) +
+  geom_point(aes(x = n, y = k2tree, color = "K2Tree"), size = 3) +
+  scale_x_continuous(trans = 'log2') +
+  scale_y_continuous(trans = 'log', labels = label_comma(accuracy = 1)) + # Format Y-axis labels without decimals
+  labs(title = "BitMatrix and K2Tree Size vs. n",
+       x = "n (Size of Matrix)",
+       y = "Size (bytes)") +
+  scale_color_manual(values = c("BitMatrix" = "green", "K2Tree" = "orange")) +
+  theme_minimal() +
+  theme(text = element_text(size = 12),
+        axis.title = element_text(size = 14),
+        axis.text = element_text(size = 10),
+        plot.title = element_text(size = 16, face = "bold"))
+
+print(space_plot)
+
+ggsave("img/SPACE.png", plot = space_plot, width = 10, height = 6, dpi = 300)
