57e4fc4fb5e0f46cc77a8791261b1ef3d700b731
BGMP <jose@bgm.cl>
2024-07-22 23:41:59 -0400

    main: add space testing metrics

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,40 @@ fn test_query_time(matrix_size: usize) {
     let _ = tree.get(x, y).unwrap();
     let duration_tree = start_time_tree.elapsed();
 
-    println!("BitMatrix: {} nanoseconds", duration_matrix.as_nanos());
-    println!("K2Tree: {} nanoseconds", duration_tree.as_nanos());
+    println!("> BitMatrix: {} nanoseconds", duration_matrix.as_nanos());
+    println!("> K2Tree: {} nanoseconds", duration_tree.as_nanos());
+}
+
+///
+/// This function tests the space usage of a BitMatrix and a K2Tree.
+///
+/// The function generates a random BitMatrix of size x size and then creates a K2Tree from it.
+/// After that, it measures the space used by the BitMatrix and the K2Tree.
+///
+fn test_space(matrix_size: usize) {
+    println!("Measuring space usage ({}x{}) ...", matrix_size, matrix_size);
+
+    let mut m = BitMatrix::with_dimensions(matrix_size, matrix_size);
+
+    let normal = Normal::new(0.5, 0.9).unwrap();
+    let mut rng = thread_rng();
+
+    for _ in 0..5 {
+        let x = rng.gen_range(0..matrix_size);
+        let y = rng.gen_range(0..matrix_size);
+        let value = normal.sample(&mut rng);
+        m.set(x, y, true).unwrap();
+    }
+
+    let tree = K2Tree::from_matrix(m.clone(), 2, 2).unwrap();
+
+    let bit_matrix_size = size_of_val(&m) + (m.width * m.height + 7) / 8;
+    let k2_tree_size = size_of_val(&tree)
+        + tree.stems.len() * size_of::<u8>()
+        + tree.leaves.len() * size_of::<u8>();
+
+    println!("> BitMatrix: {} bytes", bit_matrix_size);
+    println!("> K2Tree: {} bytes", k2_tree_size);
 }
 
 fn main() {
@@ -52,5 +84,6 @@ fn main() {
 
     for &size in sizes.iter() {
         test_query_time(size);
+        test_space(size);
     }
 }
